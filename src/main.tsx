dd610cca832ba76474640f8a1ff263946bc10588
Nic Doelger <doelgern@gmail.com>
2025-10-13 17:58:20 -0400

    github pages optimization

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
+    <Router basename="/vistar-image-generator">
       <App />
     </Router>
   </StrictMode>
